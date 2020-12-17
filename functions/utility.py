0050137bc5befe8eb95637771c52719dc01c8786
Acatsama0871 <hhl0871@outlook.com>
2020-12-17 15:29:45 -0500

    Need to add model, change background, website text

--- functions/utility.py
+++ functions/utility.py
@@ -20,7 +20,6 @@ def generate_date(begin_date, end_date):
 def get_data(begin_date, end_date):
     # set database path
     pystore.set_path(os.path.join(os.getcwd(), "database"))
-    print(os.path.join(os.getcwd(), "database")) # TODO delete
 
     # load data
     features_data = pystore.store("market_data").collection("feature").item("features").to_pandas()
@@ -33,19 +32,3 @@ def get_data(begin_date, end_date):
     market_data = market_data[market_data["Date"] <= end_date]
 
     return market_data, features_data
-
-# TODO replace
-def create_plot2(a):
-    xs = range(100)
-    ys = a * np.array(list(xs))
-    plt.plot(xs, ys)
-    plt.savefig("static/images/someplot.png")
-
-# generate fake dataframe
-def create_dataframe():
-    fake_dataframe = pd.DataFrame({"a": [1.111111, 2.222222, 3.333333, 4.444444, 1.111111, 2.222222, 3.333333, 4.444444],
-                                   "b": [1.111111, 2.222222, 3.333333, 4.444444, 1.111111, 2.222222, 3.333333, 4.444444],
-                                   "c": [1.111111, 2.222222, 3.333333, 4.444444, 1.111111, 2.222222, 3.333333, 4.444444],
-                                   "d": [1.111111, 2.222222, 3.333333, 4.444444, 1.111111, 2.222222, 3.333333, 4.444444]})
-
-    return fake_dataframe
